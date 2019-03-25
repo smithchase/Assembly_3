cfe60667ce8968263577a41753486b99a0337df1
Robert Malone <46338491+mal2994@users.noreply.github.com>
2019-03-25 09:20:20 -0400

    ask for params load only
    and it accepts 00 which might be fine anyway

--- g11p3.asm
+++ g11p3.asm
@@ -5,7 +5,7 @@ title g11p3
 ; mal2994@calu.edu smi8808@calu.edu hab6525@calu.edu
 ; March 26, 2019
 ; Program 3: Multitasking Operating System Simulator
-; CTRL F5
+; PRIORITY MUST BE 1 DIGIT, EVERY OTHER PARAMETER 2 OR IT WONT WORK
 INCLUDE Irvine32.inc
 .data
 	buffersize	equ 49
@@ -45,7 +45,8 @@ INCLUDE Irvine32.inc
 	changemess	byte	"change command received",null
 	exitmess	byte	"exiting program",null
 	promptmess	byte	"type a command or type HELP for help: ",null
-	parammess	byte	"type a single parameter: ",null
+	prtymess	byte	"type priority (1-7): ",null
+	rtmess		byte	"type runtime (01-99): ",null
 	helptext1	byte	"QUIT Terminates the program",cr,lf,null
 	helptext2	byte	"HELP Provides help with the program",cr,lf,null
 	helptext3	byte	"SHOW Shows the job queue",cr,lf,null
@@ -58,6 +59,7 @@ INCLUDE Irvine32.inc
 	helptext10	byte	"through 7, and run_time is the amount of steps the job will take before it is",cr,lf,null
 	helptext11	byte	"completed from 1 through 50. When a job is loaded it is placed in the",cr,lf,null
 	helptext12	byte	"HOLD mode",cr,lf,null
+	helptext13	byte	"PRIORITY MUST BE 1 DIGIT, EVERY OTHER PARAMETER 2 OR IT WONT WORK",cr,lf,cr,lf,null
 	quitc		byte	4,"QUIT",null
 	helpc		byte	4,"HELP",null
 	loadc		byte	4,"LOAD",null
@@ -70,7 +72,7 @@ INCLUDE Irvine32.inc
 	jqtitle		byte	"STATUS    NAME      PRIORITY  RUNTIME   LOADTIME  ",cr,lf,null
 	jobq		byte	numofjobs*jobsize dup(-1)
 	;status, name, priority, runtime, loadtime
-;	jobq		byte	1,"jimmy   ",7,0,9,0,1	;no crlf neccessary											;1,jimmy,7,09,01;
+;	jobq		byte	-1,"jimmy   ",7,0,9,0,1	;no crlf neccessary											;1,jimmy,7,09,01;
 ;	jobq1		byte	1,"pamela  ",1,5,0,0,2
 ;	jobq2		byte	1,"jacob   ",7,2,0,0,2
 ;	jobq3		byte	1,"billy   ",7,2,5,0,2	
@@ -79,16 +81,18 @@ INCLUDE Irvine32.inc
 ;	jobq6		byte	-1,"margie  ",7,3,3,0,2
 ;	jobq7		byte	1,"sam     ",7,2,1,0,3
 ;	jobq8		byte	1,"myron   ",6,5,4,0,2
-;	jobq9		byte    1,"        ",0,0,0,0	
+;	jobq9		byte    1,"        ",0,0,0,0,0	
 					   ;,null
 	bytecount	dword	0
 	stackindex	sdword	-1;-4
 	numstack	dword	stacksize dup(0)
 	startofword	dword	0
 	endofword	dword	0
-	param1		byte	0
-	param2		byte	0
-	param3		byte	0
+	jobparam	byte	0,0,0,0,0,0,0,0
+	maybeleak	byte	0,0,0,0,0,0,0,0,0,0 ; a certain subroutine might be a little erroneous 
+	rtparam		byte	0,0
+	prtyparam	byte	0
+	sysclk		byte	0,0
 
 
 
@@ -96,9 +100,6 @@ INCLUDE Irvine32.inc
 main PROC
 	;mov ebx, 55
 	mov edi, 0					; clear index reg
-	mov param1, null
-	mov param2, null
-	mov param3, null
 
 CLRLP:							; clear the input buffer
 	mov buffer[edi], null
@@ -108,7 +109,8 @@ CLRLP:							; clear the input buffer
 
 ; get byte array (command & parameters) from keyboard
 PROMPT:	
-	Call CLEARbuffer
+ 	Call CLEARbuffer
+	call CLEARbuffer2
 	mov edx, offset promptmess
 	call WriteString
 
@@ -244,40 +246,76 @@ HELPCOM:
 	call WriteString
 	mov edx, offset helptext12
 	call WriteString
+	mov edx, offset helptext13
+	call WriteString
 	jmp PROMPT
 LOADCOM:
+	mov eax, 0
 	mov edx, offset loadmess
 	call WriteString
 	call Crlf
-	mov ebx, 3
-	;call LoCom						; you can get your additional parameters with number of flags in. need 3, got 1, youll have param2 & param3
-	mov al, buffer[5]	; print operand
-	call WriteChar
-	mov al, buffer[7]	; print operand
-	call WriteChar
-	mov al, buffer[9]	; print operand
-	call WriteChar
+;	mov ebx, 3
+;	call LoCom									; you can get your additional parameters with number of flags in. need 3, got 1, youll have param2 & param3
+	call haveSPACE
+	jc loadnoerr
+	mov edx, offset errmess
+	call WriteString
+	call Crlf
+	jmp loadcomf
+loadnoerr:
+	call copyBUFFL								; copy buffer to param vars
+	call copyBUFFLQ								; copy param vars to job queue
+	jmp loadcomf
+loadcomf:
 	jmp PROMPT
 RUNCOM:
 	mov edx, offset runmess
 	call WriteString
-	mov eax, endofword
-	mov al, buffer[4]	;prints operand
-	call WriteChar
+	call Crlf
+	call clearREG
+	call copyBUFFr								; copy the param from buffer
+	call LOOKforit
+	jc runnoerr
+	mov edx, offset errmess
+	call WriteString
+	call Crlf
+	jmp runcomf
+runnoerr:
+	call PutRun									; should have job# in eax at this point, because of LOOKforit
+runcomf:
 	jmp PROMPT
 HOLDCOM:
 	mov edx, offset holdmess
 	call WriteString
-	mov eax, endofword
-	mov al, buffer[5]	; print job operand
-	call WriteChar
+	call Crlf
+	call clearREG
+	call copyBUFFhk
+	call LOOKforit
+	jc holdnoerr
+	mov edx, offset errmess
+	call WriteString
+	call Crlf
+	jmp holdcomf
+holdnoerr:
+	call PutHold
+holdcomf:
 	jmp PROMPT
 KILLCOM:
 	mov edx, offset killmess
 	call WriteString
-	mov eax, endofword
-	mov al, buffer[5]	; print job operand
-	call WriteChar
+	call Crlf
+	call clearREG
+	call copyBUFFhk
+	call LOOKforit
+	jc killnoerr
+	mov edx, offset errmess
+	call WriteString
+	call Crlf
+	jmp killcomf
+killnoerr:
+	call PutKill
+killcomf:
+	jmp PROMPT
 	jmp PROMPT
 SHOWCOM:
 	mov edx, offset showmess
@@ -330,11 +368,10 @@ SHOWQueue PROC					; ebx: index, edi: job number, ecx: 0-10 char count for field
 	mov edx, 0
 sq100:
 	inc edx
-	cmp edx, 10					; compare to 10 jobs written
-	jg endsq100					; after 10, ~move along now~
-	mov al, jobq[ebx]			; write status
-	call WriteChar
-	mov ecx, 9
+	cmp edx, 9					; compare to 10 jobs written
+	jg endsq100					; after 10, loop is done
+	mov al, jobq[ebx]			; status
+	call sq100minus				; handle the negative sign or lack thereof
 	call SPACEwriter
 	
 	inc ebx						; name index
@@ -343,40 +380,56 @@ sq100:
 	mov ecx, 2					; name is always 8, write 2 more spaces
 	call SPACEwriter
 	
-	push edx
-	dec edx						; 0 based numbering
-	imul edx, 14
-	add edx, 9
-	mov ebx, edx				; index of priority
-	pop edx
-	mov al, jobq[ebx]			; write priority
-	call WriteChar
+	;inc ebx
+	mov al, jobq[ebx]
+	add al, 30h
+	Call WriteChar
 	mov ecx, 9
-	call SPACEwriter
-	
-	inc ebx
-	mov al, jobq[ebx]			; write runtime 1st byte
-	call WriteChar
-	inc ebx
-	mov al, jobq[ebx]			; write runtime 2nd byte
-	call WriteChar
+	Call SPACEwriter
+
+	mov ecx, 2					; three loops
+tailTABLEwr:
+	push ecx
+	inc ebx						; digit 1 of PRIORITY  RUNTIME   LOADTIME  
+	mov al, jobq[ebx]
+	add al, 30h
+	Call WriteChar
+	inc ebx						; digit 2
+	mov al, jobq[ebx]
+	add al, 30h
+	Call WriteChar
 	mov ecx, 8
 	call SPACEwriter
-	
-	inc ebx
-	mov al, jobq[ebx]			; write loadtime 1st byte
-	call WriteChar
-	inc ebx
-	mov al, jobq[ebx]			; write loadtime 2nd byte
-	call WriteChar
+	pop ecx
+	dec ecx
+	cmp ecx, 0
+	jg tailTABLEwr
 	call Crlf
-
 	inc ebx
-	;inc edx			;dec edx
 	jmp sq100
 endsq100:
+
 	ret
 SHOWQueue ENDP
+
+sq100minus PROC
+	cmp al, 255					; is it minus?
+	jne sq100pos				; no, leave
+	mov al, 02Dh
+	call WriteChar				; yes, write minus
+	mov al, 1					; load a 1 and let the caller take care of writing it
+	add al, 30h					; convert digit to ascii
+	call WriteChar				; write it (-1)
+	mov ecx, 8					; write spaces, 9 even if its negative todo
+	jmp endsq100s
+sq100pos:
+	mov al, jobq[ebx]			; status
+	add al, 30h					; convert digit to ascii
+	call WriteChar				; write it
+	mov ecx, 9					; write spaces, 9 even if its negative todo
+endsq100s:
+	ret
+sq100minus ENDP
 
 SPACEwriter PROC				; use ecx for number of spaces
 beginspwr:
@@ -425,7 +478,6 @@ CLEARbuffer PROC
 	mov edi, 49
 cbloop:
 	mov buffer[edi], null
-	mov buffer2[edi], null
 	dec edi
 	cmp edi, 0
 	jg cbloop
@@ -433,6 +485,19 @@ cbloop:
 
 	ret
 CLEARbuffer ENDP
+
+CLEARbuffer2 PROC
+	push edi
+	mov edi, 49
+cb2loop:
+	mov buffer2[edi], null
+	dec edi
+	cmp edi, 0
+	jg cb2loop
+	pop edi
+
+	ret
+CLEARbuffer2 ENDP
 
 LoCom PROC					; ebx - params flag(caller function sets different ones)		INPUT ebx to how many parameters you need		; THIS SHOULD WORK FOR ANY FUNCTION THAT WASNT GIVEN ALL ITS PARAMS
 	cmp ebx, 0
@@ -470,42 +535,45 @@ LCdecp3:
 	dec ebx
 	jmp LCp3r
 LCgetp1:						; error check their parameters somewhere else
-	mov edx, offset parammess
+;	mov edx, offset parammess
 	call WriteString
 	call CLEARbuffer
+	call CLEARbuffer2
 	mov edx, offset buffer2		; address of buffer into edx
 	mov ecx, buffersize
 	call ReadString				; keyboard into buffer
 	push eax
 	mov al, buffer2[1]
 	sub eax,30h
-	mov param1, al
+;	mov param1, al
 	pop eax
 	jmp LCp1r
 LCgetp2:
-	mov edx, offset parammess
+;	mov edx, offset parammess
 	call WriteString
 	call CLEARbuffer
+	call CLEARbuffer2
 	mov edx, offset buffer2		; address of buffer into edx
 	mov ecx, buffersize
 	call ReadString				; keyboard into buffer
 	push eax
 	mov al, buffer2[1]
 	sub eax,30h
-	mov param2, al
+	;mov param2, al
 	pop eax
 	jmp LCp2r
 LCgetp3:
-	mov edx, offset parammess
+;	mov edx, offset parammess
 	call WriteString
 	call CLEARbuffer
+	call CLEARbuffer2
 	mov edx, offset buffer2		; address of buffer into edx
 	mov ecx, buffersize
 	call ReadString				; keyboard into buffer
 	push eax
 	mov al, buffer2[1]
 	sub eax,30h
-	mov param3, al
+	;mov param3, al
 	pop eax
 	jmp LCp3r
 	
@@ -519,6 +587,8 @@ IncEbx PROC
 	ret
 IncEbx ENDP
 
+;returns job#*jobsize in AL & also in stackindex
+; carry means there is a space available for a job
 haveSPACE PROC			; carry means there is a space available for a job
 	stc
 	call FindAVAILA
@@ -526,13 +596,13 @@ haveSPACE PROC			; carry means there is a space available for a job
 	je hsNOAVAILA
 	jmp hsEND
 hsNOAVAILA:
-	mov stackindex, 0
+	;mov stackindex, 0
 	clc
 hsEND:
 	ret
 haveSPACE ENDP
 
-findAVAILA PROC
+findAVAILA PROC ;returns job#*jobsize in AL
 	mov eax, 0
 	mov stackindex, 0
 	mov edi, 0
@@ -553,24 +623,235 @@ faEND:
 	ret
 findAVAILA ENDP
 
+; call:	job name in buffer variable, al is what job to check
+; return:	carry flag set if job is found
+haveJOB PROC
+; calulate offset from jobq[0]
+	mov bl, jobsize						; ax = job# * jobsize
+	mul bl								; ax = product
 
-findAVAILAold PROC
-	mov eax, 0
-	mov edi, offset jobq
-	mov stackindex, edi
-
-	mov ebx, stackindex
-	add ebx, numofjobs*jobsize			; terminate condition
-faLOOP:
-	cmp stackindex, ebx
-	je faEND
-	mov al, byte ptr stackindex[0]
-	cmp stackindex[0],0
-	jl faEND
-	mov eax, jobsize
-	add stackindex, eax
-	jmp faLOOP
-faEND:
+; move address of job table to edi for cmpsb
+	add eax, offset jobq			; jobq index 0 plus job number
+	add eax, 1						; skip over status byte
+	mov edi, eax					; cmpsb destination is each job name
+; move address of keyboard input parameter to esi for cmpsb
+	mov esi, offset jobparam		; cmpsb source is buffer
+	mov ecx, sizename
+	cld 
+	repe cmpsb
+	stc								; haveJOB will return carry high unless it gets cleared from here on out
+	je endHJ						; z flag from cmpsb is set?
+	clc
+endHJ:
 	ret
-findAVAILAold ENDP
+haveJOB ENDP
+
+clearREG PROC
+	mov eax, 0
+	mov ebx, 0
+	mov ecx, 0
+	mov edx, 0
+	mov edi, 0
+	mov esi, 0
+	ret
+clearREG ENDP
+
+copyBUFFL PROC	;for load command
+	; copy job name from buffer to jobparam
+	mov esi, offset buffer[5]
+	mov edi, offset jobparam
+	mov ecx, sizename
+	cld
+	rep movsb
+
+	mov eax, 0
+	; copy priority 
+	mov al, buffer[14]
+	sub al, 030h
+cp1P1get:
+
+	; validate
+	cmp al, 7
+	jg cp1err1
+	cmp al, 1
+	jl cp1err1
+	jmp cp1P1good
+cp1err1:
+	mov edx, offset prtymess
+	call WriteString
+	mov edx, offset buffer2		; address of buffer into edx
+	mov ecx, buffersize
+	call ReadString				; keyboard into buffer
+	mov ebx, eax
+	mov al, buffer2[0]
+	cmp ebx, 1
+	jne cp1err1
+	sub al, 030h
+	jmp cp1P1get					; when we asked for 1 param, did they give us one char? validate it
+	jmp cp1err1
+
+cp1P1good:
+	mov prtyparam, al
+cp1p1d1get:	
+	;call CLEARbuffer2
+; copy run time digit 1
+	mov al, buffer[16]
+	sub al, 030h
+;cp1p1d1get:
+; validate
+	cmp al, 9
+	jg cp2err1
+	cmp al, 0
+	jl cp2err1
+	jmp cp2P2d1good
+cp2P2d1good:
+	mov rtparam[0], al			; save valid digit 1
+	mov al, buffer[17]
+	;sub al, 030h
+; validate
+cp1p2d2get:
+	sub al, 030h
+	cmp al, 9
+	jg cp2err1
+	cmp al, 0
+	jl cp2err1
+	mov rtparam[1], al
+	jmp cplend
+cp2err1:
+	mov edx, offset rtmess
+	call WriteString
+	mov edx, offset buffer2		; for ReadString
+	mov ecx, buffersize			; for ReadString
+	call ReadString				; keyboard into buffer2
+	mov ebx, eax				; store byte count
+	mov al, buffer2[0]
+	cmp ebx, 2
+	jne cp2err1					; when we asked for 1 param, did they give us 2 chars? validate them
+;	call CLEARbuffer2
+	; copy run time
+	mov al, buffer2[0]
+	;sub al, 030h
+	mov buffer[16], al
+	mov al, buffer2[1]
+	;sub al, 030h
+	mov buffer[17], al
+	jmp cp1p1d1get
+cplend:
+	ret
+copyBUFFL ENDP
+
+copyBUFFLQ PROC
+	; load it in hold mode
+	mov ebx, stackindex
+	mov jobq[ebx],0
+
+	mov edi, 0
+	mov ebx, 0
+	; copy job name from jobparam to job queue
+	mov ebx, stackindex
+	add ebx, 1
+	add ebx, offset jobq
+	mov edi, ebx
+	mov esi, offset jobparam
+	mov ecx, sizename
+	cld
+	rep movsb
+
+	; copy priority
+	mov ebx, stackindex
+	add ebx, 1
+	add ebx, 8
+	mov dl, prtyparam
+	mov jobq[ebx], dl
+
+	; copy runtime
+	mov ebx, stackindex
+	add ebx, 1
+	add ebx, 8
+	add ebx, 1
+	mov dl, rtparam[0]
+	mov jobq[ebx],dl
+	add ebx, 1
+	mov dl, rtparam[1]
+	mov jobq[ebx],dl
+
+	; copy loadtime
+	mov ebx, stackindex
+	add ebx, 1
+	add ebx, 8
+	add ebx, 1
+	add ebx, 2
+	mov dl, sysclk[0]
+	mov jobq[ebx],dl
+	add ebx, 1
+	mov dl, sysclk[1]
+	mov jobq[ebx],dl
+
+
+	ret
+copyBUFFLQ ENDP
+
+; be sure that buffer has the correct amount of spaces!
+LOOKforit PROC ;eax returns job#
+	clc
+	mov eax, -1
+	;push eax
+lookloop:
+	;pop eax
+	inc eax
+	cmp eax, 10
+	je endlook
+	push eax
+	Call haveJOB
+	pop eax
+	jc endlook
+	;;inc eax
+	;sub eax, offset jobq		;haveJOB increments it and also adds this offset. so just take the offset off. but cant do it in the fn cuz sub sets c flag
+	jmp lookloop
+endlook:
+	ret
+	
+LOOKforit ENDP
+
+copyBUFFr PROC	;for run command. they only need a job param
+	; copy job name from buffer to jobparam
+	mov esi, offset buffer[4]
+	mov edi, offset jobparam
+	mov ecx, sizename
+	cld
+	rep movsb
+	ret
+copyBUFFr ENDP
+
+copyBUFFhk PROC	;for hold and kill commands. they only need a job param
+	; copy job name from buffer to jobparam
+	mov esi, offset buffer[5]
+	mov edi, offset jobparam
+	mov ecx, sizename
+	cld
+	rep movsb
+	ret
+copyBUFFhk ENDP
+
+PutRun PROC
+	mov bl, jobsize				; ax = job# * jobsize
+	mul bl						; ax = product
+	mov jobq[eax], 1
+	ret
+PutRun ENDP
+
+PutHold PROC
+	mov bl, jobsize				; ax = job# * jobsize
+	mul bl						; ax = product
+	mov jobq[eax], 0
+	ret
+PutHold ENDP
+
+PutKill PROC
+	mov bl, jobsize				; ax = job# * jobsize
+	mul bl						; ax = product
+	mov jobq[eax], -1
+	ret
+PutKill ENDP
+
 end
