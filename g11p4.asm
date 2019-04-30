e82bf5a078b201407463f16f240be6651e557d7c
Robert Malone <46338491+mal2994@users.noreply.github.com>
2019-04-30 09:15:45 -0400

    morning its due doesnt work

--- g11p4.asm
+++ g11p4.asm
@@ -23,6 +23,7 @@ INCLUDE Irvine32.inc
 	PacketSize	EQU	6		; how many chars
 	QUEUESIZE	EQU 6
 	NUMOMSGS	EQU	6
+
 	; packet offsets ;
 	Dest		EQU	0		; byte
 	Sender		EQU	1		; byte
@@ -35,7 +36,7 @@ INCLUDE Irvine32.inc
 	TAB			EQU	9
 
 	AXMTB		label	byte
-	BRCVA		byte	PacketSize dup(0)
+	BRCVA		byte	PacketSize dup(0);51,51,51,51,51,51;PacketSize dup(0)
 	BXMTA		label	byte
 	ARCVB		byte	PacketSize dup(0)
 
@@ -74,8 +75,10 @@ INCLUDE Irvine32.inc
 	DXMTF		label	byte
 	FRCVD		byte	PacketSize dup(0)
 
+	nullPacket	byte	PacketSize dup(0)	; never to be changed
+
 	Network		label	byte
-	NodeA		byte	'A'			; 0 name
+	NodeA		byte	'A'			; 0  name
 				byte	2			; 1  how many connections
 				dword	QUEUEA		; 2  startqueue (holds 6 chars)
 				dword	QUEUEA		; 6  inqueue pointer points to the input data in node's queue			; THIS IS THE INPTR HERE!!! IT CHANGES.
@@ -166,7 +169,7 @@ INCLUDE Irvine32.inc
 				dword	FRCVE
 	EndNetwork	byte	0
 
-	QUEUEA		byte	51,51,51,51,51,51,(NUMOMSGS-1)*PacketSize dup(0)
+	QUEUEA		byte	52,52,52,52,52,52,(NUMOMSGS-1)*PacketSize dup(0)
 	QUEUEB		byte	NUMOMSGS*PacketSize dup(0)
 	QUEUEC		byte	NUMOMSGS*PacketSize dup(0)
 	QUEUED		byte	NUMOMSGS*PacketSize dup(0)
@@ -174,59 +177,199 @@ INCLUDE Irvine32.inc
 	QUEUEF		byte	NUMOMSGS*PacketSize dup(0)
 
 				;print msgs
-	mProcSource	byte	TAB,				"Processing outgoing queue of #.",0
-	mTimeIs		byte						"Time is #.",0
-	mGotMsg		byte	TAB,TAB,			"At time # a message came from #.",0
-	mMsgMade	byte	TAB,TAB,TAB,		"A message is generated for #.",0
-	mMsgSent	byte	TAB,TAB,TAB,TAB,	"The message was sent.",0
-	mMsgSentNot	byte	TAB,TAB,TAB,TAB,	"The message was not sent.",0
-	mNewMsg		byte	TAB,TAB,			"There are # new messages.",0
-	mMsgsAct	byte	TAB,TAB,			"There are # messages active and # messages have been generated.",0
+	mProcSource	byte	TAB,				"Processing outgoing queue of #.",0										;main
+	mProcRecv	byte	TAB,TAB,			"Processing receiver buffer of #.",0									;main
+	mTimeIs		byte						"Time is #.",0															;main
+	mGotMsg		byte	TAB,TAB,			"At time # a message came from #.",0											;puttit
+	mMsgMade	byte	TAB,TAB,TAB,		"A message is generated for #.",0										;puttit
+	mMsgRec		byte	TAB,TAB,			"A message was received from #.",0										;gettit
+	mMsgSent	byte	TAB,TAB,TAB,TAB,	"The message was sent.",0												;puttit
+	mMsgSentNot	byte	TAB,TAB,TAB,TAB,	"The message was not sent.",0											;dont
+	mNewMsg		byte	TAB,TAB,			"There are # new messages.",0											;main
+	mMsgsActGen	byte	TAB,TAB,			"There are # messages active and # messages have been generated.",0		;main
+	mMsgsAct	byte	TAB,TAB,TAB,		"There are currently # messages active.",0								;main
 
 	msgcnt		byte	0
-	time		byte	0
+	time		byte	1
 
+	tempname	byte	0	; oof
+	tempcount	byte	0
+
+	maincount	byte	3
+	numonodes	byte	0
 
 .code
 main PROC
-	;debug some addresses
-	mov edi, 0
-	mov edi, offset NodeA
-;	mov edi, outpr[edi]
-	mov edi, offset AXMTB
-	mov edi, 0
 
-	mov edx, offset QUEUEA
-	mov ecx, PacketSize
-	call WriteString
-	call Crlf
-	
-	call clearreg
-	mov edi, offset NodeA
+mainloop:
+	cmp maincount, 0
+	je endmain
 	mov eax, 0
-	call PuttIt
+	mov ebx, 0
 
-	mov edx, offset AXMTB
-	mov ecx, PacketSize
-	call WriteString
-	call Crlf
+sendloop:
+		mov edi, offset Network
+		mov al, numonodes
+		push eax
+		mov eax, 0
+sendloop2:
+			cmp numonodes,0		; current node * nodesize
+			je endsendloop2
+			mov al, CONNECTIONS[edi]
+			mov bl, SIZEOFVAR
+			mul bl
+			add edi, eax
+			add edi, SIZEOFFIXED
+			dec numonodes
+			jmp sendloop2
+endsendloop2:
+		pop eax
+		mov numonodes, al
+		mov eax, 0
 
-	mov edi, offset NodeB
+		cmp numonodes, 6
+		jg sendend
+		mov eax, 0
+		mov al, numonodes
+		mov ebx, 0
+		mov bl, SIZEOFVAR
+		mul bl
+		Call PuttIt
+		inc numonodes
+		jmp sendloop
+sendend:
+	mov numonodes, 0
 	mov eax, 0
-	call Gettit
+	mov edi, 0
+	mov esi, 0
+rcvloop:
 
-	mov edx, offset mTimeIs
-	mov ecx, 8
-	call WriteString
-	call Crlf
+		mov esi, offset Network
+		mov al, numonodes
+		push eax
+		mov eax, 0
+rcvloop2:
+			cmp numonodes,0		; current node * nodesize
+			je endrcvloop2
+			mov al, CONNECTIONS[esi]
+			mov bl, SIZEOFVAR
+			mul bl
+			add esi, eax
+			add esi, SIZEOFFIXED
+			dec numonodes
+			jmp rcvloop2
+endrcvloop2:
+		pop eax
+		mov numonodes, al
+		mov eax, 0
 
-	mov edx, offset QueueB
-	mov ecx, PacketSize
-	call WriteString
-	call Crlf
-
+		cmp numonodes, 6
+		jg rcvend
+		mov eax, 0
+		mov al, numonodes
+		Call GettIt
+		inc numonodes
+		jmp rcvloop
+rcvend:
+		dec maincount
+		je mainloop
+endmain:
+	Call Crlf
+	Call Crlf
 
 main ENDP
+
+; in:	edi: node# address that will xmt
+;		eax: connection#	(0....step by sizeofvar)
+PuttIt PROC	; put data in message
+	push edi					; beginning of fixed portion of this node
+	mov bl, SIZEOFVAR			; account for VARIABLE portion of node data structure
+	mul bl
+	add eax, SIZEOFFIXED		; account for FIXED porition of node data structure
+	mov esi, OUTPTR[edi]		; SOURCE for movsb
+	push esi					; push this for null packet later
+
+	push eax					; print msg
+	mov eax, 0
+	mov al, byte ptr [edi]
+	mov tempname, al			; connection name for printing
+	pop eax
+
+	add edi, eax				;;;;;;; IN THE MAIN LOOP STEP BY SIZEOFVAR OR IT WONT WORK
+	mov edi, XMTOFFSET[edi]		; final destination
+	push esi					; see if the queue is empty
+	mov esi, [esi]
+	cmp esi, 0					
+	pop esi
+	je puttitend				; is it empty? if not, copy it
+
+	mov eax, 0
+	mov al, tempname
+	Call pMsgMade
+
+	mov ecx, PacketSize
+	cld
+	rep movsb					; copy QUEUE at OUTPTR to XMTbuf
+	
+	Call pMsgSent
+
+	pop esi						; overwrite QUEUE with Null
+	push esi
+	mov edi, esi
+	mov esi, offset NullPacket
+	mov ecx, PacketSize
+	cld
+	rep movsb
+puttitend:
+	pop esi						
+	pop edi
+	mov OUTPTR[edi], esi		; point outptr right before the null packet
+	ret
+PuttIt ENDP
+
+; in:	esi: node# address that will rcv
+;		eax: connection#
+; do one connection of one node (1-based connections)
+GettIt PROC
+	;push esi						; ptr this node
+	mov edi, esi					; ptr this node
+	mov edi, INPTR[edi]				; ptr this node's QUEUE INPTR is DESTINATION
+	add esi, SIZEOFFIXED			; ptr this node's var portion
+gettit1:							; add SIZEOFVAR until you get the eax-specified connection rcvbuf
+	cmp eax, 1				
+	je gettitdone1
+	add esi, SIZEOFVAR
+	dec eax
+	jmp gettit1
+gettitdone1:
+	mov esi, RCVOFFSET[esi]			; ptr to ptr to this node's eax-specified connection's rcvbuf
+
+	;check empty
+	push esi
+	mov esi, [esi]
+	cmp esi, 0
+	pop esi
+	je gettitend
+	;print msg
+	Call pMsgRec
+
+	push esi						; for nulling out later
+	mov ecx, PacketSize				; copy the whole packet and no more
+	cld
+	rep movsb						; copy rcvbuf packet to QUEUE at INPTR
+	add byte ptr [edi], PacketSize	; increment INPTR
+	
+	pop esi							; write null packet
+	mov edi, esi
+	mov esi, offset NullPacket
+	mov ecx, PacketSize
+	cld
+	rep movsb
+
+gettitend:
+
+	ret
+GettIt ENDP
 
 ;in:	edi points to beginning of a node
 ;out:	edi points to beginning of next node (alphabetically)
@@ -239,21 +382,6 @@ nextNode PROC
 	add edi, SIZEOFFIXED
 	ret
 nextNode ENDP
-
-cpQtoXMT PROC
-	push edi											; dont lose node pointer
-;;	mov esi, outqueue[edi]								; xmt
-	; DID NOT FINISH
-cpQtoXMT ENDP
-
-XMTptrEDX PROC
-	mov edx, 0
-	mov al, sizeofvar
-	mul bl
-;;	mov edx, xmtoffsetwithfixed[edi+eax]
-	; did not finish...?
-	ret
-XMTptrEDX ENDP
 
 ; in: AL: node#
 pProcSource PROC
@@ -273,6 +401,75 @@ pProcSource PROC
 	pop edx
 	ret
 pProcSource ENDP
+
+; in: AL: node#
+pProcRecv PROC
+	push edx
+	push ecx
+	push eax
+	mov edx, offset mProcRecv
+	add edx, sizeof mProcRecv
+	sub edx, 3
+	mov [edx], al
+	mov edx, offset mProcRecv
+	mov ecx, sizeof mProcRecv		; get end of string
+	call WriteString
+	call Crlf
+	pop eax
+	pop ecx
+	pop edx
+	ret
+pProcRecv ENDP
+
+; in: AL: node#
+pMsgMade PROC
+	push edx
+	push ecx
+	push eax
+	mov edx, offset mMsgMade
+	add edx, sizeof mMsgMade
+	sub edx, 3
+	mov [edx], al
+	mov edx, offset mMsgMade
+	mov ecx, sizeof mMsgMade		; get end of string
+	call WriteString
+	call Crlf
+	pop eax
+	pop ecx
+	pop edx
+	ret
+pMsgMade ENDP
+
+; in: AL: node#
+pMsgRec PROC
+	push edx
+	push ecx
+	push eax
+	mov edx, offset mMsgRec
+	add edx, sizeof mMsgRec
+	sub edx, 3
+	mov [edx], al
+	mov edx, offset mMsgRec
+	mov ecx, sizeof mMsgRec			; get end of string
+	call WriteString
+	call Crlf
+	pop eax
+	pop ecx
+	pop edx
+	ret
+pMsgRec ENDP
+
+; in: AL: node#
+pMsgSent PROC
+	mov edx, offset mMsgSent
+	mov ecx, sizeof mMsgSent		; get end of string
+	call WriteString
+	call Crlf
+	pop eax
+	pop ecx
+	pop edx
+	ret
+pMsgSent ENDP
 
 ; in: AL: time
 pTimeIs PROC
@@ -303,51 +500,5 @@ clearreg PROC
 	mov esi, 0
 	ret
 clearreg ENDP
-
-; in:	edi: node# address that will xmt
-;		eax: connection#
-PuttIt PROC	; put data in message
-	push edi
-	; account for variable portion of node data structure
-	;al = connection#
-	mov bl, SIZEOFVAR
-	mul bl
-	; account for fixed porition of node data structure
-	add eax, SIZEOFFIXED
-	mov esi, OUTPTR[edi]
-	add edi, eax ;;;;; you need to make this work for not 0 i think.....i would look at applying sizeofvar to this somehow
-	mov edi, XMTOFFSET[edi]
-	; destination this node's queue
-	
-	mov ecx, PacketSize
-	cld
-	rep movsb
-	pop edi
-	ret
-PuttIt ENDP
-
-; in:	edi: node# address that will rcv
-;		eax: connection#
-GettIt PROC
-	push edi
-	; account for variable portion of node data structure
-	;al = connection#
-	mov bl, SIZEOFVAR
-	mul bl
-	; account for fixed porition of node data structure
-	add eax, SIZEOFFIXED
-	push edi
-	add edi, eax
-	mov esi, RCVOFFSET[edi]
-	pop edi
-	mov edi, INPTR[edi]
-	; destination this node's queue
-	
-	mov ecx, PacketSize
-	cld
-	rep movsb
-	pop edi
-	ret
-GettIt ENDP
 
 end
